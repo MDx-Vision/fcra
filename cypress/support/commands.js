6eae2726348bad6dada99d62b727c2e6cb6f8b87
doubler <153053217+mdxvision@users.noreply.github.com>
2025-12-04 01:57:05 +0000

    Use cy.request for login

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,8 +1,12 @@
 Cypress.Commands.add('login', (email = 'test@example.com', password = 'testpass123') => {
-  cy.clearCookies();
-  cy.visit('/staff/login');
-  cy.get('input[type="email"]:visible, input[name="email"]:visible').first().type(email);
-  cy.get('input[type="password"]:visible').first().type(password);
-  cy.get('button[type="submit"]:visible').first().click();
-  cy.url().should('include', '/dashboard');
+  cy.request({
+    method: 'POST',
+    url: '/staff/login',
+    form: true,
+    body: {
+      email: email,
+      password: password
+    }
+  });
+  cy.visit('/dashboard');
 });
